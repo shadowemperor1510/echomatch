e3f97835d4188e9f055c8b15e15c843c9d9aae2f
shadowemperor1510 <adityaaditya@iitgn.ac.in>
2025-07-22 17:56:08 +0530

    app package created

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+'''
+Make app a Python Package and run Streamlit from the project root.
+For app to be treated as a Python package, it must contain an __init__.py file. 
+This file can be empty, but its presence signals to Python that the directory is a package.
+'''
